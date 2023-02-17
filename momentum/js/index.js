c740bd0be9f3c9135e7f231ed0c32b9635b2cc58
Alyona8891 <besssta888@mail.ru>
2023-02-17 15:15:58 +0300

    feat: add greeting in project

--- momentum/js/index.js
+++ momentum/js/index.js
@@ -1,6 +1,6 @@
 const time = document.querySelector('.time');
 const dateOfDay = document.querySelector('.date');
-
+const greeting = document.querySelector('.greeting');
 function showTime() {
   const date = new Date();
   const currentTime = date.toLocaleTimeString();
@@ -16,3 +16,31 @@ function showDate() {
   setTimeout(showDate, 1000);
 }
 showTime();
+function getTimeOfDay() {
+  const date = new Date();
+  const hours = date.getHours();
+  if (hours < 6) {
+    return 'night';
+  } else if (hours < 12) {
+    return 'morning';
+  } else if (hours < 18) {
+    return 'afternoon'
+  } else {
+    return 'evening';
+  }
+  setTimeout(getTimeOfDay, 1000);
+} 
+const timeOfDay = getTimeOfDay()
+greeting.textContent = `Good ${timeOfDay},`;
+const input = document.querySelector('.name');
+input.placeholder = '[Enter name]';
+function setLocalStorage() {
+  localStorage.setItem('name', input.value);
+}
+window.addEventListener('beforeunload', setLocalStorage);
+function getLocalStorage() {
+  if(localStorage.getItem('name')) {
+    input.value = localStorage.getItem('name');
+  }
+}
+window.addEventListener('load', getLocalStorage);
