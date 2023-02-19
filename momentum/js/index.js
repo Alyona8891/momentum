6a01e2ba82d6b19ddb8c509ea54967fdc254536f
Alyona8891 <besssta888@mail.ru>
2023-02-19 13:06:54 +0300

    feat: add slider of images in project

--- momentum/js/index.js
+++ momentum/js/index.js
@@ -1,6 +1,8 @@
 const time = document.querySelector('.time');
 const dateOfDay = document.querySelector('.date');
 const greeting = document.querySelector('.greeting');
+const body = document.body;
+let randomNum;
 function showTime() {
   const date = new Date();
   const currentTime = date.toLocaleTimeString();
@@ -44,3 +46,42 @@ function getLocalStorage() {
   }
 }
 window.addEventListener('load', getLocalStorage);
+
+function getRandomNum(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+randomNum = getRandomNum(1, 20)
+function setBg() {
+  
+  let timeOfDay = getTimeOfDay(); 
+  let bgNum =  randomNum.toString().padStart(2, '0');
+  const img = new Image();
+  img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`;
+  img.onload = () => {      
+    document.body.style.backgroundImage = `url(${img.src})`;
+  }; 
+}
+setBg();
+
+function getSlideNext() {
+  if (+randomNum !== 20) {
+      randomNum = [+randomNum + 1].toString().padStart(2, '0');
+  } else if (+randomNum === 20) {
+      randomNum = '01';
+  }
+  setBg();
+}
+function getSlidePrev() {
+  if (+randomNum !== 1) {
+      randomNum = [+randomNum - 1].toString().padStart(2, '0');
+  } else  {
+      randomNum = '01';
+  }
+  setBg();
+}
+const slideNext = document.querySelector('.slide-next')
+const slidePrev = document.querySelector('.slide-prev')
+slideNext.addEventListener('click', getSlideNext)
+slidePrev.addEventListener('click', getSlidePrev)
